008776e56c14f98fba4b9df1ece2310a788a9284
singhutp-glitch <singhutp@gmail.com>
2025-10-18 15:37:50 +0530

    Make project and connect tasks

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 import "./styles.css";
 console.log('hello');
-
-class task
+class project
 {
     constructor(title)
     {
         this.title=title;
     }
+    taskList=[];
+    addTask(title,description, dueDate ,priority)
+    {
+        this.taskList.push(new task(title,description, dueDate ,priority));
+    }
+}
+class task
+{
+    constructor(title,description, dueDate ,priority)
+    {
+        this.title=title;
+        this.description=description;
+        this.dueDate=dueDate;
+        this.priority=priority;
+    }
     showTask()
     {
         console.log(this.title);
+        console.log(this.description);
+        console.log(this.dueDate);
+        console.log(this.priority);
     }
 }
 
 //main
-const task1=new task("test prep");
-task1.showTask();
+const project1=new project("midsem");
+project1.addTask("test prep","maths test","1-1-25",1);
+project1.taskList[0].showTask();
