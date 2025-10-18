bcf0ae4bb9dd48d92ffe49f1c38e14956fae9249
singhutp-glitch <singhutp@gmail.com>
2025-10-18 16:41:17 +0530

    Add project module

--- src/index.js
+++ src/index.js
@@ -1,54 +1,8 @@
 import "./styles.css";
+import { projectList } from "./project";
+
 console.log('hello');
-class project
-{
-    constructor(title)
-    {
-        this.title=title;
-    }
-    taskList=[];
-    addTask(title,description, dueDate ,priority)
-    {
-        this.taskList.push(new task(title,description, dueDate ,priority));
-    }
-    deleteTask(num)
-    {
-        this.taskList.splice(num,1);
-        console.log(this.taskList.length);
-    }
-}
-class task
-{
-    constructor(title,description, dueDate ,priority)
-    {
-        this.title=title;
-        this.description=description;
-        this.dueDate=dueDate;
-        this.priority=priority;
-    }
-    showTask()
-    {
-        console.log(this.title);
-        console.log(this.description);
-        console.log(this.dueDate);
-        console.log(this.priority);
-    }
-}
-class projectList
-{
-    constructor()
-    {}
-    projList=[];
-    addProject(title)
-    {
-        this.projList.push(new project(title));
-    }
-    deleteProject(num)
-    {
-        this.projList.splice(num,1);
-        console.log(this.projList.length);
-    }
-}
+
 //main
 const listOfProjects=new projectList();
 listOfProjects.addProject("midsem");
