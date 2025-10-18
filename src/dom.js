8fb03ff5f1fbb004b0418e033142acf963832249
singhutp-glitch <singhutp@gmail.com>
2025-10-18 17:49:00 +0530

    Add dom stuff module

---
+++ src/dom.js
@@ -0,0 +1,35 @@
+export {domStuff}
+
+class domStuff
+{
+    constructor(projectItem)
+    {
+        for( let project of projectItem.projects)
+        {
+            const projectCard=document.createElement('div');
+            projectCard.textContent=project.title;
+            projectsDiv.appendChild(projectCard)
+        }
+        this.displayProject(projectItem.projects[0]);
+
+    }
+    displayProject(project)
+    {
+        if(project.taskList.length===0)
+        {
+            tasksDiv.textContent="NO Tasks";
+        }
+        else
+        {
+            for(let task of project.taskList)
+            {
+                const taskCard=document.createElement('div');
+                taskCard.textContent=task.title;
+                tasksDiv.appendChild(taskCard);
+            }
+        }
+    }
+}
+
+const projectsDiv=document.querySelector('div.projects');
+const tasksDiv=document.querySelector('div.tasks');
